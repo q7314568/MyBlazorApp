6d58b86911e681cd24c6889db874f0cab5bf32e1
q7314568 <q7314568@gmail.com>
2020-09-11 16:04:34 +0800

    推送

--- MyBlazorApp/Server/Startup.cs
+++ MyBlazorApp/Server/Startup.cs
@@ -6,6 +6,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System.Linq;
+using MyBlazorApp.Server.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace MyBlazorApp.Server
 {
@@ -22,7 +24,7 @@ namespace MyBlazorApp.Server
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-
+            services.AddDbContext<ApplicationDBContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
             services.AddControllersWithViews();
             services.AddRazorPages();
         }
